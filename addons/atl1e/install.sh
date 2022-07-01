f0941ba423572e6ec4264deb7736ca6508ad5431
Fabio Belavenuto <belavenuto@gmail.com>
2022-07-01 19:28:10 -0300

    Separating addons to another repository

--- addons/atl1e/install.sh
+++
@@ -1,4 +0,0 @@
-if [ "${1}" = "rd" ]; then
-  echo "Installing module for Atheros L1E Gigabit Ethernet adapter"
-  ${INSMOD} "/modules/atl1e.ko" ${PARAMS}
-fi
--- addons/r8125/install.sh
+++
@@ -1,4 +0,0 @@
-if [ "${1}" = "rd" ]; then
-  echo "Installing module for RealTek RTL8125 2.5Gigabit PCI-e Ethernet adapter"
-  ${INSMOD} "/modules/r8125.ko" ${PARAMS}
-fi
