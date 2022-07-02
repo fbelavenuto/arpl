995b93cf30756eb9c2e20d1e244fbbf22a3e16d0
Fabio Belavenuto <belavenuto@gmail.com>
2022-07-02 15:26:34 -0300

    Organizing for actions

--- img-gen.sh
+++ img-gen.sh
@@ -15,12 +15,31 @@ rm -rf "${BR_VER}/output/target/opt/arpl"
 rm -rf "${BR_VER}/board/arpl/overlayfs"
 rm -rf "${BR_VER}/board/arpl/p1"
 rm -rf "${BR_VER}/board/arpl/p3"
+
 # Get latest LKMs
 echo "Getting latest LKMs"
 TAG=`curl -s https://api.github.com/repos/fbelavenuto/redpill-lkm/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3)}'`
 curl -L "https://github.com/fbelavenuto/redpill-lkm/releases/download/${TAG}/rp-lkms.zip" -o /tmp/rp-lkms.zip
 rm -rf files/board/arpl/p3/lkms/*
 unzip /tmp/rp-lkms.zip -d files/board/arpl/p3/lkms
+
+# Get latest addons and install its
+echo "Getting latest Addons"
+TAG=`curl -s https://api.github.com/repos/fbelavenuto/arpl-addons/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3)}'`
+curl -L "https://github.com/fbelavenuto/arpl-addons/releases/download/${TAG}/addons.zip" -o /tmp/addons.zip
+rm -rf addons
+mkdir -p addons
+unzip /tmp/addons.zip -d addons
+DEST_PATH="files/board/arpl/p3/addons"
+echo "Installing addons to ${DEST_PATH}"
+for PKG in `ls addons/*.addon`; do
+  ADDON=`basename ${PKG} | sed 's|.addon||'`
+  mkdir -p "${DEST_PATH}/${ADDON}"
+  echo "Extracting ${PKG} to ${DEST_PATH}/${ADDON}"
+  tar xaf "${PKG}" -C "${DEST_PATH}/${ADDON}"
+done
+
+# Copy files
 echo "Copying files"
 cp -Ru files/* "${BR_VER}/"
 VERSION=`cat VERSION`
