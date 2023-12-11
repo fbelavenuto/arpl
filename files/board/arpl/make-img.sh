e792e3fc897cd0909f7f5eb71d7b745edd3f87d2
zhaozong.zzz <zhaozong.zzz@alibaba-inc.com>
2023-12-11 11:55:11 +0800

    update image to 3G

--- files/board/arpl/make-img.sh
+++ files/board/arpl/make-img.sh
@@ -15,15 +15,16 @@ BOARD_PATH="${CONFIG_DIR}/board/arpl"
 
 echo "Creating image file"
 # Create image zeroed
-dd if="/dev/zero" of="${IMAGE_FILE}" bs=1M count=1024 conv=sync 2>/dev/null
+dd if="/dev/zero" of="${IMAGE_FILE}" bs=1M count=3072 conv=sync 2>/dev/null
 # Copy grub stage1 to image
 dd if="${BOARD_PATH}/grub.bin" of="${IMAGE_FILE}" conv=notrunc,sync 2>/dev/null
 # Create partitions on image
-echo -e "n\np\n\n\n+50M\na\nt\n\n0b\nn\np\n\n\n+50M\nn\np\n\n\n\nw" | fdisk "${IMAGE_FILE}" >/dev/null
+echo -e "n\np\n\n\n+50M\na\nt\n\n0b\nn\np\n\n\n+50M\nn\np\n\n\n+2G\nn\np\n\n\nw" | fdisk "${IMAGE_FILE}" >/dev/null
 
 # Force umount, ignore errors
 sudo umount "${BINARIES_DIR}/p1" 2>/dev/null || true
 sudo umount "${BINARIES_DIR}/p3" 2>/dev/null || true
+sudo umount "${BINARIES_DIR}/p4" 2>/dev/null || true
 # Force unsetup of loop device
 sudo losetup -d "/dev/loop8" 2>/dev/null || true
 # Setup the loop8 loop device
@@ -32,12 +33,15 @@ sudo losetup -P "/dev/loop8" "${IMAGE_FILE}"
 sudo mkdosfs -F32 -n ARPL1 "/dev/loop8p1"    >/dev/null 2>&1
 sudo mkfs.ext2 -F -F -L ARPL2 "/dev/loop8p2" >/dev/null 2>&1
 sudo mkfs.ext4 -F -F -L ARPL3 "/dev/loop8p3" >/dev/null 2>&1
+sudo mkfs.ext4 -F -F -L System "/dev/loop8p4" >/dev/null 2>&1
 
 echo "Mounting image file"
 mkdir -p "${BINARIES_DIR}/p1"
 mkdir -p "${BINARIES_DIR}/p3"
+mkdir -p "${BINARIES_DIR}/p4"
 sudo mount /dev/loop8p1 "${BINARIES_DIR}/p1"
 sudo mount /dev/loop8p3 "${BINARIES_DIR}/p3"
+sudo mount /dev/loop8p4 "${BINARIES_DIR}/p4"
 
 echo "Copying files"
 sudo cp "${BINARIES_DIR}/bzImage"            "${BINARIES_DIR}/p3/bzImage-arpl"
@@ -49,7 +53,9 @@ sync
 echo "Unmount image file"
 sudo umount "${BINARIES_DIR}/p1"
 sudo umount "${BINARIES_DIR}/p3"
+sudo umount "${BINARIES_DIR}/p4"
 rmdir "${BINARIES_DIR}/p1"
 rmdir "${BINARIES_DIR}/p3"
+rmdir "${BINARIES_DIR}/p4"
 
 sudo losetup --detach /dev/loop8
